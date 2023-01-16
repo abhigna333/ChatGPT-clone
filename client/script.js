102e5acd144602f4294ad8badbfdcca6e8832c2a
abhigna333 <abhignareddy333@gmail.com>
2023-01-16 13:12:48 +0530

    Changed server link

--- client/script.js
+++ client/script.js
@@ -79,7 +79,7 @@ const handleSubmit = async (e) => {
     loader(messageDiv);
 
     //fetch data from server
-    const response = await fetch('http://localhost:5000', {
+    const response = await fetch('https://chatgpt-clone-mdd0.onrender.com/', {
         method: "POST",
         headers: {
             "Content-Type": "application/json"
